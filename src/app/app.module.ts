6f96cb556e95865248b8429203a0f6dcb34f484c
FrancoQuezada1 <francojere14@gmail.com>
2021-10-11 15:49:48 -0300

    primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { EditorModule } from 'primeng-lts/editor';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { PrincipalComponent } from './principal/principal.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    PrincipalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    EditorModule  
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/principal/principal.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css']
+})
+export class PrincipalComponent  {
+
+  usuario: string;
+  foto: string;
+  publicaciones: string;
+  noticias: string;
+  builds: string;
+
+
+  constructor() { }
+  }
+
+
