a1925541f45b237ef8d27b96fbc28d4a667d3c79
FrancoQuezada1 <francojere14@gmail.com>
2021-10-13 18:12:52 -0300

    registro añadido

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
   { path : "", component: AppComponent, pathMatch: "full"},
   { path: "login", component: LoginComponent, pathMatch: "full"},
-  { path: "principal", component: PrincipalComponent }
+  { path: "principal", component: PrincipalComponent },
+  { path: "register" , component: RegisterComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { PrincipalComponent } from './principal/principal.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    PrincipalComponent
+    PrincipalComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
